fceb569f98f1e95839e8f56492254964fcd35a60
zoefiri <pyral222@gmail.com>
2022-02-27 00:05:24 -0600

    default lib provided wm_state
    this only stores a list of window geometries at the moment but in the
    future it'll be more intricate.

---
+++ wm_state.h
@@ -0,0 +1,15 @@
+#ifndef  WIN_STATE_H
+#define  WIN_STATE_H
+
+typedef struct {
+   int x;
+   int y;
+   int w;
+   int h;
+} win_geom_t;  
+
+typedef struct {
+   win_geom_t *win_geoms;
+} wm_state_t;
+
+#endif
