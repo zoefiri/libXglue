8f6e4cc0f164fc8004fde9004d571f70cc84be01
zoefiri <pyral222@gmail.com>
2022-02-27 17:30:09 -0600

    added active window and root window state

--- src/wm_state.h
+++ src/wm_state.h
@@ -1,6 +1,8 @@
 #ifndef  WM_STATE_H
 #define  WM_STATE_H
 
+#include <xcb/xcb.h>
+
 typedef struct {
    int x;
    int y;
@@ -10,6 +12,8 @@ typedef struct {
 
 typedef struct {
    win_geom_t *win_geoms;
+   xcb_window_t active_win;
+   xcb_window_t root;
 } wm_state_t;  
 
 #endif
