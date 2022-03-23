b51685c23b9eb7ff01c8d1ee76da4f8a3878818c
zoefiri <pyral222@gmail.com>
2022-03-23 13:51:26 -0500

    added several window auxillary functions
    added aux functions for moving, managing, and destroying windows.

--- src/win_aux.c
+++ src/win_aux.c
@@ -1,7 +1,17 @@
 #include <xcb/xcb.h>
 #include <stdlib.h>
 
+#include "wm_state.h"
+
+xcb_window_t calc_updated_focus(xcb_connection_t *c, wm_state_t *wm_state) { 
+   if(wm_state->ewmh_state.clients.len)
+      return wm_state->ewmh_state.clients.clients[wm_state->ewmh_state.clients.len-1]->win;
+   else
+      return 0x0;
+}
+
 xcb_translate_coordinates_reply_t  *calc_absolute_pos(xcb_connection_t *c, xcb_window_t win, xcb_window_t root, int x, int y) {
+   printf("calculating root of %x\n", win);
    xcb_query_tree_cookie_t reply_cookie   =  xcb_query_tree(c, win);
    xcb_query_tree_reply_t  *win_tree      =  xcb_query_tree_reply(c, reply_cookie, NULL);
 
@@ -22,4 +32,70 @@ xcb_translate_coordinates_reply_t  *calc_absolute_pos(xcb_connection_t *c, xcb_w
    }
    free(win_tree);
    free(dst_coord);
+}
+
+int mvWindow(xcb_connection_t *c, xcb_window_t win, uint32_t geom[4]) {
+   xcb_configure_window(c, win, XCB_CONFIG_WINDOW_X|XCB_CONFIG_WINDOW_Y|XCB_CONFIG_WINDOW_WIDTH|XCB_CONFIG_WINDOW_HEIGHT, geom);
+   return 0;
+}
+
+int closeWindow(xcb_connection_t *c, xcb_window_t win, wm_state_t *wm_state) {
+   xcb_kill_client(c, win);
+   if(wm_state->ewmh_state.clients.len > 0) {
+      clientList_t newList;
+      int foundFlag = 0;
+      newList.len = wm_state->ewmh_state.clients.len - 1;
+      newList.clients = calloc(newList.len, sizeof(client_t*));
+      for(int i=0; i < wm_state->ewmh_state.clients.len; i++) {
+         if(i < newList.len) {
+            if(foundFlag || wm_state->ewmh_state.clients.clients[i]->win != win) {
+               printf("i:%d, setting newList:i-1 from ewmhClients:i\n", i);
+               newList.clients[i-foundFlag] = wm_state->ewmh_state.clients.clients[i];
+            }
+            else {
+               printf("i:%d, found win 0x%x\n", i, win);
+               foundFlag = 1;
+               free(wm_state->ewmh_state.clients.clients[i]);
+            }
+         }
+         else if(wm_state->ewmh_state.clients.clients[i]->win == win) {
+            printf("i:%d, on last element of ewmhClients, found 0x%x\n", i, win);
+            foundFlag = 1;
+            free(wm_state->ewmh_state.clients.clients[i]);
+         }
+      }
+      if(!foundFlag) { // win not found
+         printf("win 0x%x not found, returning with 1\n", win);
+         free(newList.clients);
+         return 1;
+      }
+      printf("writing newList to ewmhClients...\n");
+      free(wm_state->ewmh_state.clients.clients);
+      wm_state->ewmh_state.clients = newList;
+   }
+   else {
+      return 1;
+   }
+
+   return 0;
+}
+
+int manageWindow(xcb_connection_t *c, xcb_window_t win, desktop_t *desktop, wm_state_t *wm_state) {
+   printf("managing window 0x%x %d\n", win);
+   clientList_t newList;
+   newList.len = wm_state->ewmh_state.clients.len + 1;
+   newList.clients = calloc(newList.len, sizeof(client_t*));
+
+   client_t *newClient = malloc(sizeof(client_t));
+   newClient->win = win;
+   newClient->desktop = desktop;
+
+   if(wm_state->ewmh_state.clients.clients) {
+      for(int i=0; i < wm_state->ewmh_state.clients.len; i++)  newList.clients[i] = wm_state->ewmh_state.clients.clients[i];
+      free(wm_state->ewmh_state.clients.clients);
+   }
+   newList.clients[newList.len-1] = newClient;
+   wm_state->ewmh_state.clients = newList;
+
+   return 0;
 }
--- src/win_aux.h
+++ src/win_aux.h
@@ -6,6 +6,8 @@
 
 #include <xcb/xcb.h>
 
+#include "wm_state.h"
+
 /**
  * @brief translates coordinates of a window relative to root
  *
@@ -17,5 +19,10 @@
  * @return translated coordinates relative to root
  */
 xcb_translate_coordinates_reply_t  *calc_absolute_pos(xcb_connection_t *c, xcb_window_t win, xcb_window_t root, int x, int y);
+
+int mvWindow(xcb_connection_t *c, xcb_window_t win, uint32_t geom[4]);
+int closeWindow(xcb_connection_t *c, xcb_window_t win, wm_state_t *wm_state);
+int manageWindow(xcb_connection_t *c, xcb_window_t win, desktop_t *desktop, wm_state_t *wm_state);
+xcb_window_t calc_updated_focus(xcb_connection_t *c, wm_state_t *wm_state);
 
 #endif
